dbb109851c6fc00641d0f9c47d57860c4166e1b6
Ayush Rawat <97330088+ayush156rawat@users.noreply.github.com>
2024-08-13 20:14:48 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Ayuhs Rawat &nbsp;
+            Ayush Rawat &nbsp;
             <span className='sm:block hidden'> | SE Developer</span>
           </p>
         </Link>
